396f283c8f1754fe5b2b46f0b8810c7958201e76
tnngoan <ngoan.n.tr@gmail.com>
2022-04-06 15:55:49 +0700

    footer

---
+++ components/Footer.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="">
+      <div className="flex justify-around py-7 border-t-2">
+        <p className="text-blue-500">
+          <a href="https://www.hupsooncheong.com.sg/">
+            {" "}
+            Hup Soon Cheong Services Pte Ltd
+          </a>{" "}
+          &copy; 2022
+        </p>
+        <p>
+          <span>
+            <a
+              href="https://www.hupsooncheong.com.sg/privacy"
+              className="text-blue-500"
+            >
+              Terms and policy{" "}
+            </a>{" "}
+            |
+            <a href="mailto:appsupport@hsc.sg" className="text-blue-500">
+              {" "}
+              appsupport@hsc.sg
+            </a>
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
