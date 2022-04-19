30925045b4155d72ba9553f296a1789b9b8b6729
tnngoan <ngoan.n.tr@gmail.com>
2022-04-19 15:41:24 +0700

    add shake button to small cards

--- components/cards/SmallCard.js
+++ components/cards/SmallCard.js
@@ -12,7 +12,7 @@ const SmallCard = ({ card }) => {
         <p className="text-2xl font-medium">{card.title}</p>
         <p className="text-md">{card.description}</p>
       </div>
-      <button className="rounded-lg border border-2 border-green-800 uppercase font-extrabold text-green-999 mt-4 py-4">
+      <button className="hover:animate-shake-button rounded-lg border border-2 border-green-800 uppercase font-extrabold text-green-999 mt-4 py-4 hover:text-white hover:bg-green-800 hover:border-green-800">
         {card.button}
       </button>
     </div>
--- components/cards/WideCard.js
+++ components/cards/WideCard.js
@@ -4,7 +4,7 @@ import logo from "../../assets/images/logo.png";
 
 const WideCard = ({ card, order }) => {
   return (
-    <div className="grid grid-cols-3 gap-12 py-5 my-5">
+    <div className="grid grid-cols-3 gap-12 py-6 my-6">
       <Image src={logo} width={300} height={180} />
       <div className={`col-span-2 align-center px-5 mx-5 ${order}`}>
         <div className="flex flex-col py-5">
