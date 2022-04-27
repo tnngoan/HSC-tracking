3d5b55a128815a594f2a33e13cc1fd216cccd96f
tnngoan <ngoan.n.tr@gmail.com>
2022-04-27 16:32:59 +0700

    add details page after search

--- pages/api/details.js
+++ pages/api/details.js
@@ -2,11 +2,11 @@ import { connectDB } from '../../server/controller'
 
 const handler = async (req, res) => {
   if (req.method === "GET") {
-    const returnObj = await connectDB("Container", req.body.value);
+    const returnObj = await connectDB("container", req.body.value);
     if (!!returnObj) {
-      res.status(500).json(returnObj || "undefined")
+      res.status(500).json(returnObj || "No result found!")
     } else {
-      res.status(200).json(returnObj || "undefined")
+      res.status(200).json(returnObj || "Here's what I found:")
     }
   } else {
     return res.status(405).end()
