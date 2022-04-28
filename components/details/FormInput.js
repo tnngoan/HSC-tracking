f2ab12e6e988815b5c0803734c0e617179aa09a9
tnngoan <ngoan.n.tr@gmail.com>
2022-04-28 16:19:08 +0700

    remove pattern attribute

--- components/details/FormInput.js
+++ components/details/FormInput.js
@@ -12,7 +12,7 @@ const FormInput = () => {
     return (
         <div className='grid grid-rows-4'>
             <div className='flex justify-between'>
-                <input type="text" pattern={validInput} required placeholder='Phone or Email' className='w-3/4 px-4 border border-2 rounded-md mr-2' />
+                <input type="text" required placeholder='Phone or Email' className='w-3/4 px-4 border border-2 rounded-md mr-2' />
                 <button className='border border-2 text-white bg-green-900 border-green-900 rounded-md px-4 py-3 w-1/4 ml-2'>Update me</button>
             </div>
         </div>
