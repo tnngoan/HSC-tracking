58fb547a9dd155daf5862eec87cc1ad42d8e183e
tnngoan <ngoan.n.tr@gmail.com>
2022-04-30 14:21:02 +0700

    adding policy.json

--- pages/privacy.js
+++ pages/privacy.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Intro } from '../components/policy'
 
 const privacy = () => {
   return (
-    <div>privacy</div>
+    <div>
+      <Head>
+        <meta name="keywords" content="HSC Term and Privacy" />
+      </Head>
+    </div>
   )
 }
 
