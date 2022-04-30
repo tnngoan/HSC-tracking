62e0183e82da3bcb80e83eef347f2eb58a7bc4e0
tnngoan <ngoan.n.tr@gmail.com>
2022-04-30 12:25:40 +0700

    seeding hsc.inventory

---
+++ mssql/HSC2017/seed.sql
@@ -0,0 +1,88 @@
+USE [HSC2017]
+GO
+
+INSERT INTO [dbo].[HSC_Inventory]
+    ([CntrID]
+    ,[SequenceNo]
+    ,[SequencePrefix]
+    ,[HBL]
+    ,[MQuantity]
+    ,[MType]
+    ,[MVolume]
+    ,[MWeight]
+    ,[Status]
+    ,[OwnTransport]
+    ,[TranshipmentRef]
+    ,[Note]
+    ,[Amount]
+    ,[CargoRef]
+    ,[Consignee]
+    ,[CreatedBy]
+    ,[CreatedDt]
+    ,[UpdatedBy]
+    ,[UpdatedDt]
+    ,[DelStatus]
+    ,[POD]
+    ,[StorageDate]
+    ,[Shipper]
+    ,[CargoDescription]
+    ,[LockedBy]
+    ,[LockedDt]
+    ,[LockedPC]
+    ,[ToDGWhse]
+    ,[PIC]
+    ,[OwnTransportName]
+    ,[ExportBookingStatus]
+    ,[ExpBookSeq]
+    ,[CustFileRef]
+    ,[CustInventoryRef]
+    ,[Mode]
+    ,[Agent]
+    ,[CheckStatus]
+    ,[TransporterID]
+    ,[CheckStatusStuffing]
+    ,[BlockProcess])
+VALUES
+    (0
+           , NULL
+           , NULL
+           , NULL
+           , NULL
+           , NULL
+           , NULL
+           , NULL
+           , NULL
+           , NULL
+           , NULL
+           , NULL
+           , NULL
+           , NULL
+           , NULL
+           , NULL
+           , NULL
+           , NULL
+           , NULL
+           , NULL
+           , NULL
+           , NULL
+           , NULL
+           , NULL
+           , NULL
+           , NULL
+           , NULL
+           , NULL
+           , NULL
+           , NULL
+           , NULL
+           , NULL
+           , NULL
+           , NULL
+           , NULL
+           , NULL
+           , NULL
+           , NULL
+           , NULL
+           , 0)
+GO
+
+
