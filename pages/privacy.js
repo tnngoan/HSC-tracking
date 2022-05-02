954065df431987049b60d13f86c34738980988c1
tnngoan <ngoan.n.tr@gmail.com>
2022-05-02 21:23:13 +0700

    mapout policy titles

--- pages/privacy.js
+++ pages/privacy.js
@@ -1,12 +1,21 @@
 import React from 'react'
-import { Intro } from '../components/policy'
+import { Intro, Card } from '../components/policy'
+import policy from '../pages/api/policy.json'
+import Head from 'next/head'
 
 const privacy = () => {
+  console.log("private", policy)
+  const title = Object.keys(policy)
+  console.log("title arr", title)
+  const titles = []
+  
   return (
-    <div>
+    <div className='px-28'>
       <Head>
         <meta name="keywords" content="HSC Term and Privacy" />
       </Head>
+      <Intro />
+      <Card policy={policy} />
     </div>
   )
 }
