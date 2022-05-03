76f6efb5eed9624ec241636f3c11c1d0a1fed210
tnngoan <ngoan.n.tr@gmail.com>
2022-05-03 20:52:09 +0700

    adding policy content

--- pages/privacy.js
+++ pages/privacy.js
@@ -1,21 +1,16 @@
 import React from 'react'
 import { Intro, Card } from '../components/policy'
-import policy from '../pages/api/policy.json'
+import policies from '../pages/api/policy.json'
 import Head from 'next/head'
 
 const privacy = () => {
-  console.log("private", policy)
-  const title = Object.keys(policy)
-  console.log("title arr", title)
-  const titles = []
-  
   return (
     <div className='px-28'>
       <Head>
         <meta name="keywords" content="HSC Term and Privacy" />
       </Head>
       <Intro />
-      <Card policy={policy} />
+      <Card policies={policies} />
     </div>
   )
 }
