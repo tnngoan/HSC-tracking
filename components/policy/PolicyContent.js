edd5b6f47e78539524ab2805145c6d79e18c3b18
tnngoan <ngoan.n.tr@gmail.com>
2022-05-05 17:15:30 +0700

    printing ordered list

--- components/policy/PolicyContent.js
+++ components/policy/PolicyContent.js
@@ -1,12 +1,25 @@
 import React from 'react'
 
 const PolicyContent = ({ policies }) => {
-    console.log(policies)
     return (
         <div>
             {policies.map((p, i) => {
                 return (
-                    <p className='cursor-pointer font-bold'><a href="/privacy" key={i}>{Object.keys(p)}</a></p>
+                    <details key={i}>
+                        <summary className='cursor-pointer font-bold'>{Object.keys(p)}</summary>
+                        {p[Object.keys(p)].map((item, id) => {
+                            {
+                                if (typeof item === 'string') { return item } else {
+                                    return <details key={id}>
+                                        <summary className='cursor-pointer font-semibold'>
+                                            {Object.keys(item)}
+                                        </summary>
+                                    </details>
+                                }
+                            }
+                            { console.log((item)) }
+                        })}
+                    </details>
                 )
             })}
         </div>
