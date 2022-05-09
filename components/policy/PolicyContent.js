6e67a26ce57579b28f613aef7bce79c4e1d8eb23
tnngoan <ngoan.n.tr@gmail.com>
2022-05-09 22:50:58 +0700

    add full policy content

--- components/policy/PolicyContent.js
+++ components/policy/PolicyContent.js
@@ -11,42 +11,47 @@ const PolicyContent = ({ policies }) => {
                             {
                                 if (typeof item === 'string') {
                                     return (
-                                        <div key={idx} className='capitalize list-decimal font-semibold text-lg px-4 pt-2'>
-                                            <p className="capitalize">{(i + 1) + "." + (idx + 1) + ". " + item}</p>
+                                        <div key={idx} className='list-decimal font-semibold text-lg px-4 pt-2'>
+                                            <p>{(i + 1) + "." + (idx + 1) + ". " + item}</p>
                                         </div>
                                     )
                                 } else {
                                     return (
                                         <details key={idx} className='px-4 pt-2' open={true}>
-                                            <summary className='capitalize flex cursor-pointer font-semibold text-lg'>
+                                            <summary className='flex cursor-pointer font-semibold text-lg'>
                                                 {(i + 1) + "." + (idx + 1) + ". " + Object.keys(item)}
                                             </summary>
                                             {(item[Object.keys(item)]).map((child, id) => {
                                                 if (typeof child === 'string') {
-                                                    return <li key={id} className="capitalize list-disc px-8">{child}</li>
+                                                    return <li key={id} className="list-disc px-8">{child}</li>
                                                 } else {
                                                     return (
                                                         <details key={id} className='px-4' open={true}>
-                                                            <summary className='capitalize flex cursor-pointer font-semibold text-md'>
-                                                                <li>{Object.keys(child)}</li>
+                                                            <summary className='list-disc flex cursor-pointer font-semibold text-md'>
+                                                                <li className='list-disc'>{Object.keys(child)}</li>
                                                             </summary>
                                                             {(child[Object.keys(child)]).map((grandChild, index) => {
                                                                 if (typeof grandChild === 'string') {
                                                                     return <li key={index}>{grandChild}</li>
                                                                 } else {
-                                                                    return <details key={index} className="capitalize list-disc" open={true}>
-                                                                        <summary className='capitalize flex cursor-pointer font-normal text-md'>
+                                                                    return <details key={index} className="list-disc" open={true}>
+                                                                        <summary className='flex cursor-pointer font-normal text-md'>
                                                                             <li key={index} className="list-none px-4 text-sm">{Object.keys(grandChild)}</li>
                                                                         </summary>
                                                                         {(grandChild[Object.keys(grandChild)]).map((miniContent, id) => {
                                                                             if (typeof miniContent === 'string') {
                                                                                 return <p key={id} className='font-extrabold'>{miniContent}</p>
                                                                             } else {
-                                                                                return <details key={id} className='font-semibold'>
-                                                                                    <summary>
-                                                                                        {console.log(miniContent)}
-                                                                                    </summary>
-                                                                                </details>
+                                                                                return <div className='px-8 text-sm'>
+                                                                                    {(Object.keys(miniContent)).map((key, id) => {
+                                                                                        if (key.length === 1) {
+                                                                                            console.log(key)
+                                                                                            return <li>{miniContent[key]}</li>
+                                                                                        } else {
+                                                                                            return <li>{key}</li>
+                                                                                        }
+                                                                                    })}
+                                                                                </div>
                                                                             }
                                                                         })}
                                                                     </details>
