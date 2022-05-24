6536d04b6f58742e463a4f3bb3675d753cb333fa
tnngoan <ngoan.n.tr@gmail.com>
2022-05-24 08:58:16 +0700

    removed border for content boxes

--- pages/about.js
+++ pages/about.js
@@ -15,7 +15,7 @@ export default function about() {
                     <div className='px-8 rounded'>
                         <Image className='rounded' alt="HSC trucking in 1977" src={trucking} layout="responsive" />
                     </div>
-                    <div className='border border-1 border-green-999 rounded mx-8 mt-4 '>
+                    <div className='mx-8 mt-4 '>
                         <p className='py-4 px-6'>Hup Soon Cheong Services can trace its roots back to 1977. We started off as Hup Soon Cheong Transport, providing transportation services with a few lorries.</p>
                     </div>
                 </div>
@@ -24,7 +24,7 @@ export default function about() {
                     <div className='px-8 rounded'>
                         <Image alt="HSC incorporated in 1992" src={moretrucking} layout="responsive" />
                     </div>
-                    <div className='border border-1 border-green-999 rounded mx-8 mt-4'>
+                    <div className='mx-8 mt-4'>
                         <p className='py-4 px-6'>As the world moves into containerisation, we evolved and started providing container trucking services in 1980s.</p>
                     </div>
                 </div>
@@ -33,7 +33,7 @@ export default function about() {
                     <div className='px-8 rounded'>
                         <Image alt="HSC incorporated in 1992" src={containerstation} layout="responsive" />
                     </div>
-                    <div className='border border-1 border-green-999 rounded mx-8 mt-4'>
+                    <div className=' mx-8 mt-4'>
                         <p className='py-4 px-6'>Venturing into providing Container Freight Station services in 1987 proved to be a turning point for the company.  We have since grown to be a key player in Singapore’s Container Freight Station space.</p>
                     </div>
                 </div>
@@ -42,7 +42,7 @@ export default function about() {
                     <div className='px-8 rounded'>
                         <Image alt="HSC incorporated in 1992" src={incorporated} layout="responsive" />
                     </div>
-                    <div className='border border-1 border-green-999 rounded mx-8 mt-4'>
+                    <div className='mx-8 mt-4'>
                         <p className='py-4 px-6'>Hup Soon Cheong Services was incorporated as a private limited company in 1992.</p>
                     </div>
                 </div>
@@ -51,7 +51,7 @@ export default function about() {
                     <div className='px-8 rounded'>
                         <Image alt="HSC incorporated in 1992" src={staffs} layout="responsive" />
                     </div>
-                    <div className='border border-1 border-green-999 rounded mx-8 mt-4'>
+                    <div className='mx-8 mt-4'>
                         <p className='py-4 px-6'>Our staff strength stands at 200 and we count renowned multinational logistics companies as our key customers.  Cargo that we handle flows to all parts of the world.</p>
                     </div>
                 </div>
