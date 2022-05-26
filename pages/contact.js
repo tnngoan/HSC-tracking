4187cb412b70cc145a522f6951d58a0fc1bd15e4
tnngoan <ngoan.n.tr@gmail.com>
2022-05-26 10:11:12 +0700

    remove backgroun

--- pages/contact.js
+++ pages/contact.js
@@ -7,13 +7,13 @@ const Contact = () => {
     return (
         <div>
             <Header />
-            <div className='p-8 md:px-40 lg:px-80 flex-cols text-center min-h-screen bg-green-900 items-center pb-28'>
-                <h1 className='font-bold text-3xl text-white py-12'>GET IN TOUCH</h1>
+            <div className='p-8 md:px-40 lg:px-80 flex-cols text-center min-h-screen items-center pb-28'>
+                <h1 className='font-bold text-3xl text-green-999 py-12 md:py-3 md:pt-3'>GET IN TOUCH</h1>
                 <div className='border border-2 border-green-900 p-8 bg-white my-2'>
                     <div className='flex justify-around items-center'>
                         <FontAwesomeIcon icon={faEnvelopeOpen} size="3x" className='text-green-900' />
                     </div>
-                    <form className='py-6'>
+                    <form className='py-6 md:pb-3'>
                         <div className='flex items-top justify-center py-2'>
                             <label><FontAwesomeIcon icon={faAt} size="2x" className='text-green-900 px-2' /></label>
                             <input placeholder='Email' className='border border-2 rounded border-green-999 px-2 py-1 w-full md:py-2 text-gray-600' />
@@ -22,10 +22,10 @@ const Contact = () => {
                             <label><FontAwesomeIcon icon={faMessage} size="2x" className='text-green-900 px-2' /></label>
                             <textarea placeholder='Leave us a message' className='rounded border border-2 border-green-999 p-3 w-full  text-gray-600' />
                         </div>
-                        <button type="submit" className='bg-green-900 text-white w-full md:w-3/4 py-3 rounded-md mt-4'>Submit</button>
+                        <button type="submit" className='bg-green-900 text-white w-full md:w-1/4 py-3 rounded-md mt-4 md:mt-8'>Submit</button>
                     </form>
                 </div>
-                <div className=''>
+                <div className='my-4'>
                     <div className='border border-2 border-green-900 p-8 bg-white'>
                         <FontAwesomeIcon icon={faLocationDot} size="3x" className='text-green-900' />
                         <p className="font-semibold items-center pt-6 py-2">
