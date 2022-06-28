a663e8e8db2dc6168b967b9e97a65e4efc4331bc
tnngoan <ngoan.n.tr@gmail.com>
2022-06-28 09:25:03 +0700

    modify regex validation

--- components/details/FormInput.js
+++ components/details/FormInput.js
@@ -5,42 +5,43 @@ import axios from "axios"
 const FormInput = () => {
     const router = useRouter()
     const [contactInfo, setContactInfo] = useState("")
-
-    const patterns = ["^[0-9]{8}$", "^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$"]
+    // const patterns = 
+    // console.log((/^[0-9]{8}$/).test('09090909'))
+    // console.log((/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/).test("ngoan.n.tr@gmial.com"))
+    const validateContact = (contact) => {
+        console.log(contact)
+        // if (contact.test("^[0-9]{8}$")) {
+        //     setContactInfo({ type: "P" })
+        //     console.log(contactInfo.type)
+        // }
+        // if (contact.test(
+        //     "/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/"
+        // )) {
+        //     setContactInfo({ type: "E" })
+        //     // console.log(contactInfo.type)
+        // }
+        // console.log('invalid')
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(contactInfo)
-        setContactInfo(e.target.value)
-    }
-    const isNum = (str) => {
-        const type = patterns.forEach((regex) => {
-            return regex.test(str)
-        })
-        console.log(type)
-        if (str.test("[0-9]+")) {
-            return 'P'
-        } else {
-            return 'E'
-        }
+        console.log("!!!!", e.target.value)
+        validateContact(e.target.value)
+        insert()
     }
 
-    useEffect(() => {
-        const insert = async () => {
-            const res = await axios.post(`/api/subscribe`, { [isNum(contactInfo)]: contactInfo })
-            if (res.status !== 200) {
-                router.push({ pathname: "/error" })
-            } else {
-                alert("added sucessful")
-                console.log(res)
-            }
+    const insert = async () => {
+        const res = await axios.post(`/api/subscribe`, { contactInfo })
+        if (res.status !== 200) {
+            router.push({ pathname: "/error" })
+        } else {
+            alert("added sucessful")
         }
-    }, [router]
-    )
+    }
 
     return (
         <form className='flex justify-between' onSubmit={handleSubmit}>
-            <input value={contactInfo || " "} onChange={(e) => setContactInfo(e.target.value)} required requiredError="Phone number or Email is required!" pattern={regex} placeholder='Insert email or phone number' className='w-3/4 px-4 border border-1 rounded-md mr-2' />
+            <input value={contactInfo || ""} onChange={(e) => setContactInfo(e.target.value)} required pattern={/^(?:\d{10}|\w+@\w+\.\w{2,3})$/} requirederror="Phone number or Email is required!" placeholder='Insert email or phone number' className='w-3/4 px-4 border border-1 rounded-md mr-2' />
             <button type="submit" className='border border-1 text-white bg-green-900 border-green-900 rounded-md px-4 py-3 w-1/4 ml-2 hover:ring focus:bg-green-700'>Update me</button>
         </form>
     )
